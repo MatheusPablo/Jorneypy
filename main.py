bb8b35cc325ed8652fbd6cc5f52cb01795276d99
Matheus Pablo <73856049+MatheusPablo@users.noreply.github.com>
2023-03-28 18:03:11 -0200

    add

---
+++ main.py
@@ -0,0 +1,30 @@
+# print() ; mostrar algo na tela
+print("hello word")
+print("hello"+" word")
+print("h""e""l""l""o"" ""w""o""r""d")
+
+#operaçõe aritmeticas + soma, - subtração, / divisão, * Mutiplicação, ** Exponenciação, // numero da divisão inteira, % resto da divisão
+
+
+print(2+2)
+print(3-1)
+print(8/2)
+print(2*4)
+print(2**2)
+print(15/4)
+print(7%3)
+
+#transformação de tipos de caracteres , int , float, string
+
+print(int(123.3)) 
+print(float(120))
+print(str(234 + 23))
+
+a = str(2+3)
+print(type(a))
+
+#funcoes vista em aula
+
+
+print(len("eu sou chat gpt um modelo de linguagem desnevolvido pelo opneai"))
+print(len(str(242242424242424)))
